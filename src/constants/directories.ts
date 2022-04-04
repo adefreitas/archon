b47ceb7b46334d540153fc77c37915e5c19c2f6b
Andres De Freitas <6485562+adefreitas@users.noreply.github.com>
2022-04-04 19:06:36 +0200

    chore: restructure project

--- src/constants/directories.ts
+++ src/constants/directories.ts
@@ -1,6 +1,6 @@
 export const INPUT_DIR = "input";
 export const OUTPUT_DIR = "output";
 
-export const INPUT_ASSETS_DIR = `${INPUT_DIR}/assets`;
+export const INPUT_ATTRIBUTES_DIR = `${INPUT_DIR}/attributes`;
 export const OUTPUT_FRAMES_DIR = `${OUTPUT_DIR}/frames`;
 export const OUTPUT_VIDEO_DIR = `${OUTPUT_DIR}/video`;
--- src/generators/frames.ts
+++ src/generators/frames.ts
@@ -1,14 +1,14 @@
 import * as fs from "fs";
 import * as sharp from "sharp";
-import { INPUT_ASSETS_DIR, OUTPUT_FRAMES_DIR } from "../constants/directories";
+import { INPUT_ATTRIBUTES_DIR, OUTPUT_FRAMES_DIR } from "../constants/directories";
 
 export async function generateFrames() {
   console.log("Frame generation started 🏎");
-  const backgroundPath = `./${INPUT_ASSETS_DIR}/background.png`;
+  const backgroundPath = `./${INPUT_ATTRIBUTES_DIR}/background.png`;
 
   for (let i = 1; i < 5; i++) {
-    const leftImagePath = `./${INPUT_ASSETS_DIR}/left${i}.png`;
-    const rightImagePath = `./${INPUT_ASSETS_DIR}/right${i}.png`;
+    const leftImagePath = `./${INPUT_ATTRIBUTES_DIR}/left${i}.png`;
+    const rightImagePath = `./${INPUT_ATTRIBUTES_DIR}/right${i}.png`;
     const outputFramePath = `./${OUTPUT_FRAMES_DIR}/output${i}.png`;
 
     const leftImage = fs.readFileSync(leftImagePath);
