9e5537b6da3f071a30fdaadda2209ba5d65bde4b
Andres De Freitas <6485562+adefreitas@users.noreply.github.com>
2022-04-26 22:46:19 +0200

    chore: cleaning up deprecated code

--- src/generators/asset.ts
+++
@@ -1,9 +0,0 @@
-import { readManifest } from "../utils/manifest";
-import { generateFrames } from "./frames";
-import { generateVideo } from "./video";
-
-export async function generateAssets() {
-  // await generateFrames(manifest);
-  // generateVideo();
-
-}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 
-import { generateAssets } from "./generators/asset";
 import { readManifest, worker } from "./utils/manifest";
 
 async function main() {
   console.log("Hello there! Starting processing");
-  generateAssets();
   const manifest = readManifest();
   await worker(0, 60, manifest);
 }
